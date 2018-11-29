24d1f583bd3f30a6e3171fbceddf71e36eb1bb6f
Jurgita <Jurgita.pupalaigiene@gmail.com>
2018-11-29 13:14:13 +0200

    su Produkto klase

---
+++ src/Produktas.js
@@ -0,0 +1,11 @@
+class Produktas {
+    constructor(title, description, price, quantity){
+        this.title=title;
+        this.description=description;
+        this.price=price;
+        this.quantity=quantity;
+    }
+
+}
+
+export default Produktas;
